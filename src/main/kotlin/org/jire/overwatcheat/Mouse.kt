71f18cb6fef26e7555df3a7d08902ad01282cda1
Orwénnes <40231164+EdLovecraft@users.noreply.github.com>
2025-06-25 16:30:31 +0800

    Update Mouse.kt

--- src/main/kotlin/org/jire/overwatcheat/Mouse.kt
+++ src/main/kotlin/org/jire/overwatcheat/Mouse.kt
@@ -18,50 +18,11 @@
 
 package org.jire.overwatcheat
 
-import org.jire.overwatcheat.nativelib.interception.Interception.interceptionContext
-import org.jire.overwatcheat.nativelib.interception.Interception.interceptionMouseStrokeLayout
-import org.jire.overwatcheat.nativelib.interception.Interception.interception_send
-import org.jire.overwatcheat.nativelib.interception.InterceptionFilter
-import org.jire.overwatcheat.nativelib.interception.InterceptionMouseFlag
-import java.lang.Thread.sleep
-import java.lang.foreign.MemorySegment
-import java.lang.foreign.MemorySession
-import java.lang.foreign.ValueLayout
-
 object Mouse {
-
-    val mouseStroke =
-        MemorySegment.allocateNative(interceptionMouseStrokeLayout, MemorySession.global()).apply {
-            set(ValueLayout.JAVA_SHORT, 0, 0) // state
-            set(ValueLayout.JAVA_SHORT, 2, 0) // flags
-            set(ValueLayout.JAVA_SHORT, 4, 0) // rolling
-            set(ValueLayout.JAVA_INT, 8, 0) // x
-            set(ValueLayout.JAVA_INT, 12, 0) // y
-            set(
-                ValueLayout.JAVA_SHORT, 14,
-                (InterceptionMouseFlag.INTERCEPTION_MOUSE_MOVE_RELATIVE or
-                        InterceptionMouseFlag.INTERCEPTION_MOUSE_CUSTOM).toShort()
-            ) // information
-        }
-
     fun move(x: Int, y: Int, deviceID: Int) {
-        mouseStroke.run {
-            set(ValueLayout.JAVA_INT, 8, x)
-            set(ValueLayout.JAVA_INT, 12, y)
-        }
-        interception_send(interceptionContext, deviceID, mouseStroke, 1)
+        SerialMouse.move(x, y)
     }
-
     fun click(deviceID: Int) {
-        mouseStroke.run {
-            set(ValueLayout.JAVA_INT, 0, InterceptionFilter.INTERCEPTION_MOUSE_LEFT_BUTTON_DOWN)
-            set(ValueLayout.JAVA_INT, 8, 0)
-            set(ValueLayout.JAVA_INT, 12, 0)
-        }
-        interception_send(interceptionContext, deviceID, mouseStroke, 1)
-        sleep(300)
-        mouseStroke.set(ValueLayout.JAVA_INT, 0, InterceptionFilter.INTERCEPTION_MOUSE_LEFT_BUTTON_UP)
-        interception_send(interceptionContext, deviceID, mouseStroke, 1)
+        SerialMouse.click()
     }
-
 }
